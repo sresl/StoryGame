e9707c3d9747a556915f2a379c33e1ad59e77a0a
Twanghofer <tobias.w01@gmx.at>
2018-12-04 09:03:14 +0100

    added RandomApfelStates and Sprites

--- Unity/TextAdventure_MEP/Assets/MyGame/Scripts/RandomState.cs
+++ Unity/TextAdventure_MEP/Assets/MyGame/Scripts/RandomState.cs
@@ -3,7 +3,7 @@
 
 public class RandomState{
     //for refactoring and or debugging
-    public static readonly Random getrandom = new Random(123);
+    public static readonly Random getrandom = new Random();
 
     //for gameplay
     //public static readonly Random getrandom = new Random();
